71a7ad19665bcb7247c93fa47c5aa0f6b65d5a5f
Nicola Kaleta <99048749+nic00la1@users.noreply.github.com>
2025-12-09 11:27:04 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,9 +11,9 @@
 // ================ Sortowanie Bąbelkowe ===============
 
 
-int[] t = {3, 2 ,4 , 1, 5, 6};
+int[] t = {3, 2 ,4 , 1, 7, 6};
 
-/*
+
 void BubbleSort(int[] t) 
 {
     for (int i = 0; i < t.Length - 1; i++) 
@@ -40,11 +40,12 @@ void BubbleSort(int[] t)
     }
 }
 
-BubbleSort(t);
-*/
+// BubbleSort(t);
+
 
 // ================ Sortowanie przez wstawianie ===============
 
+/*
 void InsertSort(int[] t)
 {
     for (int i = 1; i < t.Length; i++)
@@ -67,3 +68,140 @@ void InsertSort(int[] t)
 }
 
 InsertSort(t);
+*/
+
+// Console.WriteLine("Tablica początkowa: ");
+// Console.WriteLine(string.Join(" ", t));
+// Console.WriteLine();
+
+// SelectionSort(t);
+
+void SelectionSort(int[] t)
+{
+    for (int i = 0; i < t.Length - 1; i++)
+    {
+        int minIndex = i;
+
+        for (int j = i + 1; j < t.Length; j++) 
+        {
+            if(t[j] < t[minIndex]) 
+            {
+                minIndex = j;
+            }
+        }
+
+        Console.WriteLine($"Zamieniamy miejscami {t[minIndex]} z {t[i]}");
+
+        int temp = t[minIndex];
+        t[minIndex] = t[i];
+        t[i] = temp;
+
+        // pokazanie całej tablicy w jednej linii
+        Console.WriteLine(string.Join(" ", t));
+    }
+}
+
+// ============= Wyszukiwanie liniowe - 1 pętla (1 warunek, 1 pętla) =============
+
+/*
+int LinearSearch(int[] t, int szukana) 
+{
+    for (int i = 0; i < t.Length; i++) 
+    {
+        if (t[i] == szukana) 
+        {
+            Console.WriteLine($"Znaleziono element w indeksie {i}");
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+LinearSearch(t, 1);
+*/
+// ============= Wyszukiwanie binarne =============
+// BubbleSort(t);
+
+int BinarySearch(int[] t, int szukana) {
+    int lewy = 0;
+    int prawy = t.Length - 1;
+
+    while(lewy <= prawy) {
+        int srodek = (lewy + prawy) / 2;
+
+        if(t[srodek] == szukana) 
+        {
+            Console.WriteLine($"Znaleziono naszą szukaną w indeksie {srodek}");
+            return srodek;
+        }
+
+        if (t[srodek] < szukana) 
+            lewy = srodek + 1;
+        else 
+            prawy = srodek - 1;
+    }
+
+    return -1;
+}
+
+// BubbleSort(t);
+// BinarySearch(t, 7);
+
+class Sprzedaz 
+{
+    private static int liczba = 0;
+    public int Id {get; set;}
+    public string Data {get; set;} = "";
+    public string Produkt {get; set;} = "";
+    public int Ilosc {get; set;}
+    public double CenaJednostkowa {get;set;}
+
+    public Sprzedaz() {
+        Id = ++liczba;
+    }
+
+    public void ShowData() 
+    {
+        Console.WriteLine($"ID: {Id}, Produkt: {Produkt}, Data: {Data}, ");
+    }
+}
+
+class Program 
+{
+    public static void Main(string[] args) 
+    {
+        List<Sprzedaz> sprzedaze = new List<Sprzedaz>();
+        using(StreamReader stream = new StreamReader("sprzedaz_300.csv")) 
+        {
+            // Pominięcie jednej linijki (nazwa naszych kolumn)
+            string line = stream.ReadLine();
+
+            // Pętla, do momentu aż nie zwróci
+            while((line = stream.ReadLine()) != null) 
+            {
+                // Dzielimy to na części
+                string[] parts = line.Split(',');
+
+                // Zamieniamy kropkę na przecinek, by działał nam Double
+                parts[3] = parts[3].Replace('.', ',');
+
+                // Dodajemy do tablicy sprzedaży
+                sprzedaze.Add(new Sprzedaz 
+                {
+                    Data = parts[0],
+                    Produkt = parts[1],
+                    Ilosc = int.Parse(parts[2]),
+                    CenaJednostkowa = double.Parse(parts[3])
+                });
+            }
+
+            for (int i = 0; i < 5; i++) 
+            {
+                sprzedaze[i].ShowData();
+            }
+        }
+    }
+}
+
+
